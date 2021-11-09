f1f31ffc482324eb0899d3336b1cdd1e688fa4cd
hosuiting <u3556933@connect.hku.hk>
2021-11-09 20:39:14 +0800

    2
    change to Button and able to go to home page

--- app/src/main/java/hk/hku/cs/comp3330project/HomeActivity.java
+++ app/src/main/java/hk/hku/cs/comp3330project/HomeActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class IntroActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        setContentView(R.layout.activity_home);
     }
 }
--- app/src/main/java/hk/hku/cs/comp3330project/MainActivity.java
+++ app/src/main/java/hk/hku/cs/comp3330project/MainActivity.java
@@ -1,14 +1,26 @@
 package hk.hku.cs.comp3330project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button startBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,HomeActivity.class));
+            }
+        });
     }
 }
