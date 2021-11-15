39081b3dde3b8069f50e77e2b480af9cc681e94a
nathanwooo <nathanwooo1231@gmail.com>
2021-11-16 01:31:28 +0800

    CaloriesCalActivity

--- app/src/main/java/hk/hku/cs/comp3330project/CaloriesCalActivity.java
+++ app/src/main/java/hk/hku/cs/comp3330project/CaloriesCalActivity.java
@@ -2,7 +2,11 @@ package hk.hku.cs.comp3330project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CaloriesCalActivity extends AppCompatActivity {
     private Button beef_button;
@@ -11,12 +15,39 @@ public class CaloriesCalActivity extends AppCompatActivity {
     private Button fish_button;
     private Button vege_button;
     private Button carbs_button;
+    private EditText beef_amount;
+    private EditText pork_amount;
+    private EditText chicken_amount;
+    private EditText fish_amount;
+    private EditText vege_amount;
+    private EditText carbs_amount;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calories_cal);
+        beef_button = (Button) findViewById(R.id.beef_button);
+        pork_button = (Button)findViewById(R.id.pork_button);
+        chicken_button = (Button)findViewById(R.id.chicken_button);
+        fish_button = (Button)findViewById(R.id.fish_button);
+        vege_button = (Button)findViewById(R.id.vege_button);
+        carbs_button = (Button)findViewById(R.id.carbs_button);
+        beef_amount = (EditText) findViewById(R.id.beef_amount);
+        pork_amount = (EditText) findViewById(R.id.pork_amount);
+        chicken_amount = (EditText) findViewById(R.id.chicken_amount);
+        fish_amount = (EditText) findViewById(R.id.fish_amount);
+        vege_amount = (EditText) findViewById(R.id.vege_amount);
+        carbs_amount = (EditText) findViewById(R.id.carbs_amount);
 
+
+
+        beef_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                beef_amount.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
--- app/src/main/java/hk/hku/cs/comp3330project/HomeActivity.java
+++ app/src/main/java/hk/hku/cs/comp3330project/HomeActivity.java
@@ -2,13 +2,24 @@ package hk.hku.cs.comp3330project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private Button toCaloriesCal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        toCaloriesCal=(Button) findViewById(R.id.toCaloriesCal);
+        toCaloriesCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this,CaloriesCalActivity.class));
+            }
+        });
+
     }
 }
