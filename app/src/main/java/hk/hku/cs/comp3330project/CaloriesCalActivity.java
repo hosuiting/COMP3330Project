25c8f57514de4aabe4f75181c7759fd1dba5350a
nathanwooo <nathanwooo1231@gmail.com>
2021-11-16 00:59:05 +0800

    CaloriesCalActivity

---
+++ app/src/main/java/hk/hku/cs/comp3330project/CaloriesCalActivity.java
@@ -0,0 +1,22 @@
+package hk.hku.cs.comp3330project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CaloriesCalActivity extends AppCompatActivity {
+    private Button beef_button;
+    private Button pork_button;
+    private Button chicken_button;
+    private Button fish_button;
+    private Button vege_button;
+    private Button carbs_button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calories_cal);
+
+    }
+}
