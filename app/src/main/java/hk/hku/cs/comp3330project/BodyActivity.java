cea63f2f8dcb5355ee0084671e07e20a5758b920
SumYg <u3568315@connect.hku.hk>
2021-11-15 16:41:17 +0800

    Add body activity

---
+++ app/src/main/java/hk/hku/cs/comp3330project/BodyActivity.java
@@ -0,0 +1,14 @@
+package hk.hku.cs.comp3330project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BodyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
