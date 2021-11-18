dbfcea63b2e6c2c4b57f5833c7e11468054448e0
SumYg <61381909+SumYg@users.noreply.github.com>
2021-11-19 02:17:04 +0800

    Use Fragment to do navigation

--- app/src/main/java/hk/hku/cs/comp3330project/BodyActivity.java
+++ app/src/main/java/hk/hku/cs/comp3330project/BodyActivity.java
@@ -3,12 +3,23 @@ package hk.hku.cs.comp3330project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BodyActivity extends AppCompatActivity {
+    private Button home_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body);
+//        home_button = findViewById(R.id.button);
+//        home_buttonsetOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                beef_kcal_text.setVisibility(View.VISIBLE);
+//                beef_amount.setVisibility(View.VISIBLE);
+//            }
+//        });
     }
 }
