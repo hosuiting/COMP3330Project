5bd77eda49513b94ee4e0f63e23ad460ffec3326
hosuiting <u3556933@connect.hku.hk>
2021-11-19 17:42:54 +0800

    Chatroom build

---
+++ app/src/main/java/hk/hku/cs/comp3330project/MsgModel.java
@@ -0,0 +1,17 @@
+package hk.hku.cs.comp3330project;
+
+public class MsgModel {
+    private String cnt;
+
+    public MsgModel(String cnt) {
+        this.cnt = cnt;
+    }
+
+    public void setCnt(String cnt) {
+        this.cnt = cnt;
+    }
+
+    public String getCnt() {
+        return cnt;
+    }
+}
---
+++ app/src/main/java/hk/hku/cs/comp3330project/RetrofitAPI.java
@@ -0,0 +1,10 @@
+package hk.hku.cs.comp3330project;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Url;
+
+public interface RetrofitAPI {
+    @GET
+    Call<MsgModel> getMessage(@Url String url);
+}
