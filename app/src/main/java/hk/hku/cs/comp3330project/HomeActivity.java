fcd10ade28660e55462c66cd732d4c3c52d153ac
nathanwooo <nathanwooo1231@gmail.com>
2021-11-23 03:47:30 +0800

    navigation bar

--- app/src/main/java/hk/hku/cs/comp3330project/HomeActivity.java
+++ app/src/main/java/hk/hku/cs/comp3330project/HomeActivity.java
@@ -1,15 +1,20 @@
 package hk.hku.cs.comp3330project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -28,6 +33,26 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.menu);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+
+                        break;
+                    case R.id.profile:
+                        startActivity(new Intent(HomeActivity.this,RegisterActivity.class));
+                        break;
+                    case R.id.chatbot:
+                        startActivity(new Intent(HomeActivity.this,ChatbotActivity.class));
+                        break;
+                }
+
+                return false;
+            }
+        });
+
         Greet = (TextView)findViewById(R.id.Greet);
         SharedPreferences mydata = getSharedPreferences("mydata", Context.MODE_PRIVATE);
         Greet.setText("Hi "+mydata.getString("username",""));
