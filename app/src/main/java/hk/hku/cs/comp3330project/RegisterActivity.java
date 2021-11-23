bb97aa5e05ad38433968be4a47200b6b1ec08fb0
nathanwooo <nathanwooo1231@gmail.com>
2021-11-23 19:20:50 +0800

    register

--- app/src/main/java/hk/hku/cs/comp3330project/RegisterActivity.java
+++ app/src/main/java/hk/hku/cs/comp3330project/RegisterActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import retrofit2.http.Body;
@@ -24,6 +25,7 @@ public class RegisterActivity extends AppCompatActivity {
     private Button confirm_button;
     private Button cancel_button;
     private RadioButton Male_button,Female_button;
+    private TextView register_message, username_text,age_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,34 +39,91 @@ public class RegisterActivity extends AppCompatActivity {
         cancel_button = (Button) findViewById(R.id.Cancel_button);
         Male_button = (RadioButton)findViewById(R.id.Male_button);
         Female_button = (RadioButton)findViewById(R.id.Female_button);
+        register_message = (TextView)findViewById(R.id.register_message);
+        username_text = (TextView)findViewById(R.id.username_text);
+        age_text = (TextView)findViewById(R.id.age_text);
+        username_text.setVisibility(View.GONE);
+        age_text.setVisibility(View.GONE);
+        SharedPreferences mydata = getSharedPreferences("mydata", Context.MODE_PRIVATE);
+        if(mydata.contains("username")){
+            register_message.setText("Edit your profile");
+            username.setText(mydata.getString("username",""));
+            age.setText(mydata.getString("age",""));
+            height.setVisibility(View.GONE);
+            weight.setVisibility(View.GONE);
+            bodyfat.setVisibility(View.GONE);
+            username_text.setVisibility(View.VISIBLE);
+            age_text.setVisibility(View.VISIBLE);
+            if(mydata.getString("sex","")=="male"){
+                Male_button.setChecked(true);
+                Female_button.setChecked(false);
+            }
+            else if(mydata.getString("sex","")=="female"){
+                Male_button.setChecked(false);
+                Female_button.setChecked(true);
+            }
+        }
+
         cancel_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(RegisterActivity.this,MainActivity.class));
             }
         });
+
         confirm_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(TextUtils.isEmpty(username.getText()) || TextUtils.isEmpty(age.getText()) ||
-                    TextUtils.isEmpty(height.getText()) || TextUtils.isEmpty(weight.getText()) || TextUtils.isEmpty(bodyfat.getText())){
+                if(TextUtils.isEmpty(username.getText()) || TextUtils.isEmpty(age.getText())){
                     Toast toast = Toast.makeText(getApplicationContext(),
                             "Please input in all the fields!",
                             Toast.LENGTH_SHORT);
 
                     toast.show();
                 }
-                else{
-                    SharedPreferences mydata = getSharedPreferences("mydata", Context.MODE_PRIVATE);
+                else if(mydata.contains("username")){
                     SharedPreferences.Editor editor = mydata.edit();
                     editor.putString("username",username.getText().toString());
                     editor.putString("age",age.getText().toString());
+                    if(Male_button.isChecked()){
+                        editor.putString("sex","male");
+                    }
+                    else if(Female_button.isChecked()){
+                        editor.putString("sex","female");
+                    }
                     editor.commit();
-
-                    BodyStatSQLiteHelper putdb = new BodyStatSQLiteHelper(RegisterActivity.this);
-                    putdb.addStatfromRegister(height.getText().toString(),weight.getText().toString(),bodyfat.getText().toString());
-
                     startActivity(new Intent(RegisterActivity.this,HomeActivity.class));
+                }
+
+
+
+                else{
+                        if(TextUtils.isEmpty(height.getText()) || TextUtils.isEmpty(weight.getText()) ||TextUtils.isEmpty(bodyfat.getText()) ){
+                            Toast toast = Toast.makeText(getApplicationContext(),
+                                    "Please input in all the fields!",
+                                    Toast.LENGTH_SHORT);
+
+                            toast.show();
+                        }
+                        else{
+                            SharedPreferences.Editor editor = mydata.edit();
+                            editor.putString("username",username.getText().toString());
+                            editor.putString("age",age.getText().toString());
+                            if(Male_button.isChecked()){
+                                editor.putString("sex","male");
+                            }
+                            else if(Female_button.isChecked()){
+                                editor.putString("sex","female");
+                            }
+                            editor.commit();
+                            BodyStatSQLiteHelper putdb = new BodyStatSQLiteHelper(RegisterActivity.this);
+                            putdb.addStatfromRegister(height.getText().toString(),weight.getText().toString(),bodyfat.getText().toString());
+
+                            startActivity(new Intent(RegisterActivity.this,HomeActivity.class));
+                        }
+
+
+
 
                 }
             }
