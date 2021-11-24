29c46202b4a3cb913cb2f0c31abe03a5feef53ad
george28911 <64721999+george28911@users.noreply.github.com>
2021-11-24 22:41:00 +0800

    merged article activity

--- app/src/main/java/hk/hku/cs/comp3330project/Article.java
+++ app/src/main/java/hk/hku/cs/comp3330project/Article.java
@@ -1,11 +1,66 @@
 package hk.hku.cs.comp3330project;
 
-public class Article {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class Article extends AppCompatActivity {
 
     String title, date;
 
     public Article(String title, String date) {
         this.title = title;
         this.date = date;
+    }
+
+    private ImageView button_like;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+        button_like = findViewById(R.id.button_like);
+        final boolean liked = false;
+
+        button_like.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                liked = !liked;
+                if(liked) {
+                    button_like.setBackgroundResource(R.drawable.heart_before_like);
+                } else if (!(liked)){
+                    button_like.setBackgroundResource(R.drawable.heart_before_like);
+                }
+            }
+        });
+
+        ((BottomNavigationView) findViewById(R.id.menu)).setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(Article.this,HomeActivity.class));
+                        break;
+                    case R.id.profile:
+                        startActivity(new Intent(Article.this,RegisterActivity.class));
+                        break;
+                    case R.id.chatbot:
+                        startActivity(new Intent(Article.this,ChatbotActivity.class));
+                        break;
+                }
+
+                return false;
+            }
+        });
+
     }
 }
