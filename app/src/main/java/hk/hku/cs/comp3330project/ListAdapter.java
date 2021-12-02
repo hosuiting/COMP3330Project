037c170ad2642d541dca7cfd27a36941a49b3b67
george28911 <64721999+george28911@users.noreply.github.com>
2021-12-02 22:01:38 +0800

    Update ListAdapter.java

--- app/src/main/java/hk/hku/cs/comp3330project/ListAdapter.java
+++ app/src/main/java/hk/hku/cs/comp3330project/ListAdapter.java
@@ -1,10 +1,12 @@
 package hk.hku.cs.comp3330project;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,10 +14,13 @@ import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
-public class ListAdapter extends ArrayAdapter<Article> {
+public class ListAdapter extends ArrayAdapter<Article>{
+
+    private Context context;
 
     public ListAdapter(Context context, ArrayList<Article> articleArraylist) {
         super(context, R.layout.list_item, articleArraylist);
+        this.context = context;
     }
 
     @NonNull
@@ -27,12 +32,17 @@ public class ListAdapter extends ArrayAdapter<Article> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
         }
 
-        //ImageView imageView =
         TextView title = convertView.findViewById(R.id.articletitle);
         TextView date = convertView.findViewById(R.id.date);
 
         title.setText(article.title);
         date.setText(article.date);
+        String image = "@drawable/" + article.images;
+
+        int imageResource = context.getResources().getIdentifier(image, null, context.getPackageName());
+        ImageView imageView = convertView.findViewById(R.id.profile_pic);
+        Drawable res = context.getResources().getDrawable(imageResource);
+        imageView.setImageDrawable(res);
 
         return super.getView(position,convertView, parent);
     }
